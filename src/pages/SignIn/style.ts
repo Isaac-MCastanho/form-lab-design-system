ce364ab0d4bb7a5c1ae4671cb4a23bebab416200
Isaac-MCastanho <fgcastanho@hotmail.com>
2022-11-22 15:03:09 -0300

    updates to sign in and header(global)

--- src/pages/SignIn/style.ts
+++ src/pages/SignIn/style.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   background-image: linear-gradient(transparent 70%, #121214 100%),
-    url("./../../public/assets/backgrounds/bg-signin.svg");
+    url("assets/backgrounds/bg-signin.svg");
   background-position: start;
   background-repeat: no-repeat;
   background-size: cover;
